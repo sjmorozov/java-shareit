731be87ea68cbeb7590f9b002c451ada0191e4b0
sjmorozov <sjmorozov@gmail.com>
2026-09-06 23:12:12 +0300

    feat: add booking creation endpoint

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,27 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.booking.dto.BookingCreateDto;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.service.BookingService;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
-@RequestMapping(path = "/bookings")
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
 public class BookingController {
+    private static final String USER_ID_HEADER = "X-Sharer-User-Id";
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto create(@RequestHeader(USER_ID_HEADER) Long userId,
+                             @Valid @RequestBody BookingCreateDto bookingCreateDto) {
+        return bookingService.create(userId, bookingCreateDto);
+    }
 }
